bae252cb13d69c52d50a30be5dd4c58a29a40a8d
timdeeter <timdeeter1@gmail.com>
2018-09-11 14:58:42 -0400

    Added event class

---
+++ HoosierVolunteer.Models/Events.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoosierVolunteer.Models
+{
+    public class Events
+    {
+        public enum EventType
+        {
+            Formal = 1,
+            Governance,
+            Nonformal,
+            SocialAction,
+            Project
+        }
+
+        [Key]
+        public Guid CreatorId { get; set; }
+
+        public DateRange EventRange { get; set; }
+        public EventType Type { get; set; }
+        public string EventTitle { get; set; }
+        public int VolunteersNeeded { get; set; }
+        public int AttendingVolunteers { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    public class DateRange
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public bool Equals(DateTime date)
+        {
+            if (date == null)
+            {
+                return false;
+            }
+            
+            if ((Start <= date) && (date <= End))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
