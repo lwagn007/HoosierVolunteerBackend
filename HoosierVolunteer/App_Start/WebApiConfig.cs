6e856c3dc8ef953948c6ee9752e9ccb4ca80d7cb
timdeeter <timdeeter1@gmail.com>
2018-09-18 16:36:44 -0400

    kms

--- HoosierVolunteer/App_Start/WebApiConfig.cs
+++ HoosierVolunteer/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace HoosierVolunteer
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
 
-            
+            config.EnableCors();
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
             
